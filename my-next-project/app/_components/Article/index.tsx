5cd3f326f269d40485ec7f711de2ffe9e7aab08f
w24010 <w24010@osfl.ac.jp>
2025-05-16 10:31:46 +0900

    6章まで完了

---
+++ my-next-project/app/_components/Article/index.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import type { News } from "@/app/_libs/microcms";
+import Date from "../Date";
+import Category from "../category";
+import styles from "./index.module.css";
+
+type Props = {
+    data : News;
+};
+
+export default function Article ({ data }: Props) {
+    return (
+        <main>
+            <h1 className={styles.title}>{data.title}</h1>
+            <p className={styles.description}>{data.description}</p>
+            <div className={styles.meta}>
+                <Category category={data.Category} />
+                <Date date={data.publishedAt ?? data.createdAt} />
+            </div>
+            {data.thumbnail && (
+                <Image
+                    src={data.thumbnail.url}
+                    alt=""
+                    className={styles.thumbnail}
+                    width={data.thumbnail.width}
+                    height={data.thumbnail.height}
+                />
+            )}
+            <div
+                className={styles.contents}
+                dangerouslySetInnerHTML={{
+                    __html:data.content,
+                }}
+            />
+        </main>
+    )
+}
